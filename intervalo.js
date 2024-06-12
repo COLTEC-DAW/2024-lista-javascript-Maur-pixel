7e1cd7c86e9a36ca3f5f65083646b885e5b544d7
Maur-pixel <mauriciofsilva01@gmail.com>
2024-06-12 13:59:20 -0300

    atv intervalo com parametro

--- intervalo.js
+++ intervalo.js
@@ -1,9 +1,15 @@
-function range(min, max) {
+function range(min, max, i) {
     let array = [];
-    for (let i = min; i <= max; i++) {
-        array.push(i);
+
+    for (let count = 0; count < i; count++) {
+        if (min == max)
+            break;
+        else {
+            array.push(min);
+            min++;
+        }   
     }
     return array;
 }
 //teste
-console.log(range(1, 10));
+console.log(range(1, 10, 2));
